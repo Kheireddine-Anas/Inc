ce25a100b49cbc34f868764a3b3b408065193fce
Kheireddine-Anas <kheireddine.anas@gmail.com>
2025-02-18 15:35:23 +0100

    Finishing Mandatory

---
+++ srcs/requirements/mariadb/conf/run_db.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Initialize the database if it doesn't exist
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    chown -R mysql:mysql /var/lib/mysql
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+
+    tfile=$(mktemp)
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
+fi
+
+echo $DB_ROOT
+echo $DB_NAME
+echo $DB_USER
+echo $DB_PASS
+
+
+# Configure the root user and create the database
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+    cat <<EOF > /tmp/create_db.sql
+USE mysql;
+FLUSH PRIVILEGES;
+DROP DATABASE IF EXISTS wordpress;
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.db WHERE Db='wordpress';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+CREATE DATABASE wordpress CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+    # Run the SQL commands
+   mariadbd --user=mysql --bootstrap < /tmp/create_db.sql
+    rm -f /tmp/create_db.sql
+fi
---
+++ srcs/requirements/wordpress/conf/wp-install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+while ! mariadb -hmariadb -u$DB_USER -p$DB_PASS $DB_NAME --silent 2> /dev/null; do
+    sleep 1;
+done
+
+# Check if WordPress is already configured
+if [ -f /var/www/html/wp-config.php ]
+then
+    echo "Wordpress already downloaded"
+else
+    echo "Downloading WordPress"
+    # Download WordPress
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
+    chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
+
+    sed -i 's#listen = /run/php/php7.4-fpm.sock#listen = 0.0.0.0:9000#' /etc/php/7.4/fpm/pool.d/www.conf
+    sed -i 's#pid = /run/php/php7.4-fpm.pid#pid = 0.0.0.0:9000#' /etc/php/7.4/fpm/php-fpm.conf
+
+    cd /var/www/html 
+    wp core download --allow-root
+    mv wp-config-sample.php wp-config.php
+
+    wp config set DB_NAME $DB_NAME --allow-root
+    wp config set DB_USER $DB_USER --allow-root
+    wp config set DB_PASSWORD $DB_PASS --allow-root
+    wp config set DB_HOST 'mariadb:3306' --allow-root
+
+    # Install WordPress
+    wp core install --url=$DOMAIN_NAME --title=$TITLE \
+    --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS \
+    --admin_email=$WP_ADMIN_EMAIL --allow-root
+
+    # Add dynamic URL configuration to wp-config.php
+    echo "Adding dynamic URL configuration to wp-config.php"
+    echo "define('WP_HOME', 'https://' . \$_SERVER['HTTP_HOST']);" >> /var/www/html/wp-config.php
+    echo "define('WP_SITEURL', 'https://' . \$_SERVER['HTTP_HOST']);" >> /var/www/html/wp-config.php
+
+    # Update siteurl and home to use the VM's IP address
+    wp option update siteurl "https://localhost" --allow-root
+    wp option update home "https://localhost" --allow-root
+
+    # Create additional users
+    wp user create $WP_USER1 $WP_EMAIL1 --role=author --user_pass=$WP_PASS1 --allow-root
+    wp user create $WP_USER1 $WP_EMAIL2 --role=author --user_pass=$WP_PASS2 --allow-root
+
+    # Set permissions
+    chown -R www-data:www-data /var/www/html/
+fi
+
+# Ensure the /run/php directory exists
+mkdir -p /run/php
+
+# Start PHP-FPM
+exec php-fpm7.4 -F
